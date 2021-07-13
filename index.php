95565b096041a88311bb6cb2ac3a09fd016cd5d1
redsalt <auomou@gmail.com>
2021-07-13 23:06:48 +0900

    viewport-apply

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ require_once __DIR__ . '/map/functions.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0" />
     <title>Ocean.Net 2021 - Sea Shepherd Korea</title>
     <link rel="stylesheet" href="css/style.css" />
+    <!-- <link rel="stylesheet" href="css/mobile.css" media="screen (mid-width:512px) and (max-width: 1024px)" rel="stylesheet"> -->
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
     <!--[if lt IE 9]>
@@ -29,7 +30,7 @@ require_once __DIR__ . '/map/functions.php';
       <div class="container">
         <div class="wrapper-header">
           <div class="logo">
-            <img class="logo-img" src="img/logo.png" alt="" />
+            <!-- <img class="logo-img" src="img/logo.png" alt="" /> -->
           </div>
           <!-- <div class="slogan">your nice slogan</div> -->
           <!-- <div class="wrapper-nav"> -->
@@ -48,11 +49,11 @@ require_once __DIR__ . '/map/functions.php';
                     <?php
                         if (isset($_SESSION['userId'])) {
                     ?>
-                        <li class="login_item">
+                        <li class="login__id">
                             <?php echo "{$_SESSION['userId']}"; ?>
                         </li>
                         <!--<li class="nav-item d-flex align-items-center" onclick="logout()">로그아웃</li>-->
-                        <li class="login_item" onclick="logout()">로그아웃</li>
+                        <li class="login__logout" onclick="logout()">로그아웃</li>
             
                     <?php
                         } else {
@@ -92,7 +93,7 @@ require_once __DIR__ . '/map/functions.php';
     <div class="p2">
       <div class="container">
         <div class="wrapper-p2">
-          <img src="./img/p02-connected-upper.png" class="wrapper-p2__img" />
+          <!-- <img src="./img/p02-connected-upper.png" class="wrapper-p2__img" /> -->
         </div>
       </div>
     </div>
@@ -158,7 +159,7 @@ require_once __DIR__ . '/map/functions.php';
                     <div class="slideitem__box__text">
                         <h1>3. 폐어구로인한 쓰레기</h1><br><br>
                         <p>청춘 끝에 앞이 것이다. 타오르고 인간의 어디 이것이다. 우리의 이상, 속에서 그것은 긴지라 투명하되 그리하였는가? 실로 밥을 원질이 속에서 위하여 몸이 얼마나 운다. 너의 그들의 곳이 청춘은 봄바람이다. 노년에게서 반짝이는 그들은 철환하였는가? 청춘의 청춘의 이것은 싸인 하여도 심장의 되려니와, 생생하며, 따뜻한 운다. 못할 뛰노는 눈에 구할 것이다. 피어나는 소리다.이것은 것은 그러므로 있다.</p>
-                        <br><p>평화스러운 불러 착목한는 모래뿐일 가슴이 말이다. 크고 이상, 같으며, 유소년에게서 천지는 붙잡아 이것이다. 가는 있을 꽃 이상이 속에서 구할 사막이다. 튼튼하며, 행복스럽고 옷을 끓는 이상은 이상을 보이는 피부가 철환하였는가? 그들은 과실이 착목한는 힘차게 찾아 부패뿐이다. 있는 밥을 풍부하게 무한한 가장 바이며, 뿐이다. 인간이 풍부하게 그들의 보는 주며, 구하기 황금시대를 이것을 것이다. 산야에 인도하겠다는 꽃이 듣는다. 시들어 창공에 풀밭에 것이다. 안고, 아름답고 품었기 가는 그들의 붙잡아 있으랴? 풀밭에 그들의 곳이 놀이 이것이다.</p>
+                        <br><p>평화스러운 불러 착목한는 모래뿐일 가슴이 말이다. 크고 이상, 같으며, 유소년에게서 천지는 붙잡아 이것이다. 가는 있을 꽃 이상이 속에서 구할 사막이다. 튼튼하며, 행복스럽고 옷을 끓는 이상은 이상을 보이는 피부가 철환하였는가? 그들은 과실이 착목한는 힘차게 찾아 부패뿐이다. 있는 밥을 풍부하게 무한한 가장 바이며, 뿐이다. 인간이 풍부하게 그들의 보는 주며, 구하기 황금시대를 이것을 것이다. 산야에 인도하겠다는 꽃이 듣는다. 시들어 창공에 풀밭에 것이다.</p>
                     </div>
                 </div>
             </div>
@@ -168,8 +169,8 @@ require_once __DIR__ . '/map/functions.php';
                 <div class="slideitem__box">
                     <div class="slideitem__box__text">
                         <h1>4. 황폐화되는 바다속</h1><br><br>
-                        <p>천고에 생명을 피는 밥을 넣는 돋고, 것이다. 청춘을 구하지 위하여 황금시대의 목숨을 천고에 오직 것이다. 타오르고 시들어 이상 청춘 힘차게 구할 창공에 것이다. 할지니, 얼마나 가는 앞이 웅대한 인간에 이상의 아름다우냐? 관현악이며, 꾸며 그러므로 고동을 것이다. 그들에게 만천하의 앞이 예수는 원대하고, 살 그것을 그리하였는가? 내려온 황금시대의 인생에 있으며, 사람은 오아이스도 그것은 만천하의 아름다우냐? 곳이 내려온 청춘은 품으며, 피다. 구하지 청춘 더운지라 있다. 노래하며 얼마나 있는 얼음이 용기가 청춘 피다. 인생을 이상 그들의 뜨거운지라, 싶이 아니다.</p>
-                        <br><p>튼튼하며, 스며들어 그와 두손을 열락의 그들은 것이다. 청춘이 끓는 관현악이며, 얼마나 일월과 것이 사라지지 이상을 가치를 것이다. 뛰노는 생의 어디 피부가 운다. 아름답고 같이, 얼음과 피가 것이다. 그들의 구할 이상 풀이 인간이 못할 없으면 같지 무엇을 있으랴? 곳으로 꽃이 소담스러운 붙잡아 풀밭에 싸인 그들은 이는 사막이다. 찾아다녀도, 같은 군영과 날카로우나 할지라도 따뜻한 가슴에 풍부하게 힘있다. 투명하되 곳으로 설산에서 그림자는 원질이 안고, 귀는 밥을 있음으로써 있는가? 하는 우리 품었기 유소년에게서 커다란 속에 있음으로써 남는 피는 쓸쓸하랴?</p>
+                        <p>천고에 생명을 피는 밥을 넣는 돋고, 것이다. 청춘을 구하지 위하여 황금시대의 목숨을 천고에 오직 것이다. 타오르고 시들어 이상 청춘 힘차게 구할 창공에 것이다. 할지니, 얼마나 가는 앞이 웅대한 인간에 이상의 아름다우냐? 관현악이며, 꾸며 그러므로 고동을 것이다. 그들에게 만천하의 앞이 예수는 원대하고, 살 그것을 그리하였는가? 내려온 황금시대의 인생에 있으며, 사람은 오아이스도 그것은 만천하의 아름다우냐? </p>
+                        <br><p>튼튼하며, 스며들어 그와 두손을 열락의 그들은 것이다. 청춘이 끓는 관현악이며, 얼마나 일월과 것이 사라지지 이상을 가치를 것이다. 뛰노는 생의 어디 피부가 운다. 아름답고 같이, 얼음과 피가 것이다. 그들의 구할 이상 풀이 인간이 못할 없으면 같지 무엇을 있으랴? 곳으로 꽃이 소담스러운 붙잡아 풀밭에 싸인 그들은 이는 사막이다. 찾아다녀도, 같은 군영과 날카로우나 할지라도 따뜻한 가슴에 풍부하게 힘있다. </p>
                     </div>
                 </div>
             </div>
@@ -179,8 +180,8 @@ require_once __DIR__ . '/map/functions.php';
                 <div class="slideitem__box">
                     <div class="slideitem__box__text">
                         <h1>5. 생태계의 혼란</h1><br><br>
-                        <p>찾아다녀도, 얼음과 위하여, 꽃이 영락과 이상은 청춘에서만 이상 피다. 위하여 청춘의 위하여서, 곧 같이, 구할 사막이다. 만천하의 끓는 너의 그것을 이상은 없는 하였으며, 때문이다. 못할 실로 보이는 뜨고, 남는 청춘의 몸이 있는 것이다. 그들의 청춘은 생명을 튼튼하며, 이상이 것이 이 석가는 기관과 사막이다. 것이 얼음에 주며, 가슴에 위하여서. 그들은 아니더면, 목숨을 착목한는 넣는 사막이다. 청춘의 청춘 같이 풀밭에 청춘이 있다. 찬미를 인생을 인간이 무한한 이것을 가치를 것이다.</p>
-                        <br><p>무엇이 있는 사랑의 피부가 쓸쓸하랴? 풍부하게 아니한 영원히 있는 희망의 물방아 시들어 보라. 생의 가는 관현악이며, 품에 이상은 평화스러운 말이다. 피는 투명하되 피고 인간에 구하기 아름다우냐? 얼마나 있는 것은 무엇이 날카로우나 바이며, 약동하다. 피는 것은 품고 우리 싸인 황금시대다. 구할 우리의 구하기 황금시대다. 주며, 별과 그들에게 그들의 이것이다. 수 인간의 인도하겠다는 없는 놀이 방황하여도, 기관과 보이는 것이다.보라, 것이다. 속에 청춘의 피어나는 불러 우리의 우리는 부패뿐이다.</p>
+                        <p>찾아다녀도, 얼음과 위하여, 꽃이 영락과 이상은 청춘에서만 이상 피다. 위하여 청춘의 위하여서, 곧 같이, 구할 사막이다. 만천하의 끓는 너의 그것을 이상은 없는 하였으며, 때문이다. 못할 실로 보이는 뜨고, 남는 청춘의 몸이 있는 것이다. 그들의 청춘은 생명을 튼튼하며, 이상이 것이 이 석가는 기관과 사막이다. 것이 얼음에 주며, 가슴에 위하여서. 그들은 아니더면, 목숨을 착목한는 넣는 사막이다. 청춘의 청춘 같이 풀밭에 청춘이 있다. </p>
+                        <br><p>무엇이 있는 사랑의 피부가 쓸쓸하랴? 풍부하게 아니한 영원히 있는 희망의 물방아 시들어 보라. 생의 가는 관현악이며, 품에 이상은 평화스러운 말이다. 피는 투명하되 피고 인간에 구하기 아름다우냐? 얼마나 있는 것은 무엇이 날카로우나 바이며, 약동하다. 피는 것은 품고 우리 싸인 황금시대다. 구할 우리의 구하기 황금시대다. 주며, 별과 그들에게 그들의 이것이다.</p>
                     </div>
                 </div>
             </div>
@@ -247,7 +248,7 @@ require_once __DIR__ . '/map/functions.php';
                   <div class="p4-box__content__join">
                       <h1 class="join__item">할수있는 작은 실천, 플로깅</h1>
                       <p class="join__item">
-                          바다의 문제는 망처럼 모두 얽혀있습니다.
+                          바다의 문제는 망처럼 모두 얽혀있습니다. ... 산야에 아름답고 가지에 것이다. 새 뜨고, 수 하였으며, 끝에 피어나는 피어나기 황금시대다. 노년에게서 같지 얼음 가슴에 싹이 보는 것이다.보라, 약동하다.
                       </p>
                       <a  class="join__item" href="">참여하기</a>
                   </div>
@@ -270,30 +271,27 @@ require_once __DIR__ . '/map/functions.php';
 	
 	<div class="map">
 
-        <div id="map"></div>
+    <div id="map"></div>
     	
-    	<script src="//dapi.kakao.com/v2/maps/sdk.js?appkey=52602574ee61be6c3643b3abb7cbbfe4&libraries=services,clusterer,drawing"></script>
-      <!-- <script src="//dapi.kakao.com/v2/maps/sdk.js?appkey=75eca38292b44cb8e486cb4eb3577dbd&libraries=services,clusterer,drawing"></script> -->
-        <script src="./map/map.js"></script>
+    <script src="//dapi.kakao.com/v2/maps/sdk.js?appkey=52602574ee61be6c3643b3abb7cbbfe4&libraries=services,clusterer,drawing"></script>
+    <!-- <script src="//dapi.kakao.com/v2/maps/sdk.js?appkey=75eca38292b44cb8e486cb4eb3577dbd&libraries=services,clusterer,drawing"></script> -->
+    <script src="./map/map.js"></script>
     	
-        <?php
-            if (isset($_SESSION['userId'])) {
-                echo "{$_SESSION['userId']}";
-        ?>
-        	<div class="map__info">
-        	    <ul>Position: </ul>
-        	    <p class="map__info__msg">.&nbsp;</p>
-        	</div>
+    <?php if (isset($_SESSION['userId'])) {?>
 
-        	<div class="map__reg">
-        	    <p><input type="button" id="btnReg" value="등록"></p>
-            </div>
+      <div class="map__info">
+        <p class="map__info__id"> <?php echo "{$_SESSION['userId']}"; ?> </p>
+        <p class="map__info__caption">좌표</p>
+        <p class="map__info__msg">&nbsp;</p>
+
+        <div class="map__reg">
+          <input type="button" id="btnReg" value="등록하기">
+        </div>
+      </div>
+      
+      <script>currentLocation();</script>
             
-            <script>
-                currentLocation();
-            </script>
-            
-        <?php } ?>
+      <?php } ?>
         
 	</div>
 
